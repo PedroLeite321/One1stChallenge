d0a37537e5888edb7b8608f26f554aab5997ea12
pedro <pk.bleite@gmail.com>
2024-01-25 16:14:29 -0300

    improved code / código melhorado

--- src/scripts/typing.js
+++ src/scripts/typing.js
@@ -1,17 +1,38 @@
-const typeAnimation = ()    =>  {
-    //
-    let i = 0;
-    let placeholder = "";
-    const txt = "Digite seu texto";
-    const typingSpeed = 60;
+// const typeAnimation = ()    =>  {
+//     //
+//     let i = 0;
+//     let placeholder = "";
+//     const txt = "Digite seu texto";
+//     const typingSpeed = 60;
 
 
-    const type = () =>  {
-        placeholder += txt.charAt(i);
-        document.getElementById('animated__input').setAttribute('placeholder', placeholder);
-        i++;
-        setTimeout(type,typingSpeed);
+//     const type = () =>  {
+//         placeholder += txt.charAt(i);
+//         document.getElementById('animated__input').setAttribute('placeholder', placeholder);
+//         i++;
+//         setTimeout(type,typingSpeed);
+//     }
+//     type();
+// }
+// typeAnimation();
+
+const main = (() =>  {
+    // encapsulation concept. Helps to prevent changes in variables as the lenght of the input will likely be static.
+    const typeAnimation = () => {
+        let i = 0;
+        let placeholder = "";
+        const txt = "Digite seu texto aqui";
+        const typingSpeed = 80;
+        console.log(i);
+        const typing = ()   =>  {
+            placeholder += txt.charAt(i) //will receive the string character by character
+            document.getElementById('animated__input').setAttribute('placeholder', placeholder)//add a placeholder with you current placeholder value to the target.
+            i++; //Makes sure that the index is always the next.
+            // executes the function typing at our defined speed
+            setTimeout(typing, typingSpeed);
+        }
+        typing();
     }
-    type();
-}
-typeAnimation();
+
+    typeAnimation();
+})();
