9a700b1aa7225074ef72b552463959959fa0cf83
pedro <pk.bleite@gmail.com>
2024-01-27 01:42:34 -0300

    cryptograph logic done / lógica da criptográfia feita

--- src/scripts/encrypt.js
+++ src/scripts/encrypt.js
@@ -28,16 +28,19 @@ const mainCrypt = () =>  {
             const getText = states.document.userMainInput.value;
             let textContent;
 
-
+            console.log("alface")
             if (getText === "" || getText === undefined || getText === null) {
                 states.document.userMainInput.setAttribute('placeholder', "");
                 textContent = "Por favor, insira algo!";
+                main(textContent);
                 
+            }else   {
+                getUserInput();
             }
             
             
 
-            main(textContent);
+           
         }
 
         const checkForInput = () => {
@@ -49,6 +52,59 @@ const mainCrypt = () =>  {
         checkForInput();
       
         const getUserInput = () => {
+            const userTxt = states.document.userMainInput.value;
+            let str = [];
+            const checkCryptoLogic = (char) =>   {
+                
+                
+                const addToStr = (newChar) =>  {
+                    str.push(newChar);
+                    console.log(str);
+                }
+               
+                switch(char)    {
+                    case 'a':
+                        char = 'ai';
+                        addToStr(char);
+                        break;
+                    case 'i':
+                        char = 'imes';
+                        addToStr(char);
+                        break;
+                    case 'e':
+                        char = 'enter';
+                        addToStr(char);
+                        break;
+                    case 'o':
+                        char = 'ober';
+                        addToStr(char);
+                        break;
+                    case 'u':
+                        char = 'ufat';
+                        addToStr(char);
+                        break;
+                    default:    
+                         addToStr(char);
+                         break;
+                }
+                
+                //get the modified string and add to the aside and write it in the placeholder aswell
+                return str;
+                
+            }
+            
+            const defineCryptograph = () => {
+                let encryptedStr;
+                for(let i = 0; i < userTxt.length; i++) {
+                    
+                    encryptedStr = checkCryptoLogic(userTxt[i]);
+                }
+                console.log(encryptedStr)
+        
+            }
+            defineCryptograph();
+            
+            
             const clearAside = () =>    {
 
             }
